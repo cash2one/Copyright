fab3d0da3ba384ce802864db11321a358ac9629e
cuiyinsheng <cuiyinsheng@baidu.com>
2016-11-04 16:34:49 +0800

    add Assignment and Parallel
    Change-Id: Ia39dffb0e35cc30566c90a53b03106469da1551b

--- actions/inner/Parallel.php
+++ actions/inner/Parallel.php
@@ -7,13 +7,44 @@
 
 /**
  * @file Parallel.php
- * @author chenzhenyu01(com@baidu.com)
+ * @author cuiyinsheng(com@baidu.com)
  * @date 2016/11/4 10:56
  * @brief  单个进程接到任务运行调度的类
  *  
  **/
  
- 
+class Action_Parallel extends Ap_Action_Abstract
+{
+    public function execute()
+    {   
+        $httpGet = $_GET;
+        $request = Saf_SmartMain::getCgi();
+        $httpPost = $request['post'];
+        $pn = $httpPost['pn'];
+        $start = $httpPost['start'];
+        $end = $httpPost['end'];
+        $casePerPage = $httpPost['casePerPage'];
+        $mode = $httpPost['mode'];
+        $type = $httpPost['type'];
+        $scope = $httpPost['scope'];
+        $query = $httpPost['query'];
+        $chapter = $httpPost['chapter'];
+        $text = $httpPost['text'];
+        
+        $unitJob = new Servie_Data_Unit();
+        $ret = $unitJob->schedule(
+            $pn, 
+            $start, 
+            $end, 
+            $casePerPage, 
+            $mode, 
+            $type, 
+            $scope, 
+            $query, 
+            $chapter, 
+            $text);
+    }   
+}
  
  /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
  ?>
