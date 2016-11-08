5b38b271a54e68788bbad593ab48aad9b825fddf
chenzhenyu01 <chenzhenyu01@baidu.com>
2016-11-08 14:54:06 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -6,16 +6,11 @@ mkdir -p output/app/$APP_NAME
 mkdir -p output/conf/app
 mkdir -p output/webroot/$APP_NAME
 mkdir -p output/php/phplib/$PRODUCT_NAME/api/$APP_NAME
-
-#拷贝代码文件
 cp -r actions controllers library models script Bootstrap.php output/app/$APP_NAME
-
-#拷贝配置文件
-cp -r conf/$APP_NAME  output/conf/app/
-#拷贝webroot index 文件
+cp -r conf/*  output/conf/app
 cp -r index.php  output/webroot/$APP_NAME
-
+cp -r api/* output/php/phplib/$PRODUCT_NAME/api/$APP_NAME
 cd output
 find ./ -name .svn -exec rm -rf {} \;
-tar cvzf $APP_NAME.tar.gz app conf webroot php
+tar cvzf copyright.tar.gz app conf webroot php
 rm -rf app conf webroot php
