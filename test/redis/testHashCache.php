0976645f7188938c531240d82e79c22ef2e5aa36
chenzhenyu01 <chenzhenyu01@baidu.com>
2016-11-08 17:09:14 +0800

    test的结构

--- test/redis/testHashCache.php
+++ test/redis/testHashCache.php
@@ -1,16 +1,16 @@
 <?php
 /***************************************************************************
- * 
+ *
  * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
- * 
+ *
  **************************************************************************/
 
 /**
  * @file testHashCache.php
  * @author chenzhenyu01(com@baidu.com)
  * @date 2016/11/8 15:18
- * @brief 
- *  
+ * @brief
+ *
  **/
 
 
@@ -26,38 +26,38 @@ testWrite();
  */
 function testWrite()
 {
- $field_value = array();
- for($i = 40;$i<49;$i++)
- {
-  $field_value[$i] = getRandStr();
- }
- $obj = new Service_Copyright_HashCache();
+    $field_value = array();
+    for ($i = 40; $i < 49; $i++) {
+        $field_value[$i] = getRandStr();
+    }
+    $obj = new Service_Copyright_HashCache();
 
- $key = getRandStr();
- var_dump('[key]'.$key);
+    $key = getRandStr();
+    var_dump('[key]' . $key);
 
- $ret = $obj->write($key,$field_value);
- return $ret;
+    $ret = $obj->write($key, $field_value);
+    return $ret;
 }
 
-function getRandStr($length=null)
+/**
+ * @param null $length
+ * @return string
+ */
+function getRandStr($length = null)
 {
- if(empty($length))
- {
-  $length = rand(10,20);
- }
+    if (empty($length)) {
+        $length = rand(10, 20);
+    }
 
- $str = '';
- $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
- $max = strlen($strPol)-1;
- for($i=0;$i<$length;$i++)
- {
-  $str .= $strPol[rand(0,$max)];
- }
- return $str;
+    $str = '';
+    $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+    $max = strlen($strPol) - 1;
+    for ($i = 0; $i < $length; $i++) {
+        $str .= $strPol[rand(0, $max)];
+    }
+    return $str;
 }
- 
- 
- 
- /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
- ?>
+
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
+?>
--- test/redis/testRead.php
+++ test/redis/testRead.php
@@ -1,16 +1,16 @@
 <?php
 /***************************************************************************
- * 
+ *
  * Copyright (c) 2015 Baidu.com, Inc. All Rights Reserved
- * 
+ *
  **************************************************************************/
 
 /**
  * @file testRead.php
  * @author chenzhenyu01(com@baidu.com)
  * @date 2016/11/8 15:21
- * @brief 
- *  
+ * @brief
+ *
  **/
 
 Bd_Init::init('copyright');
@@ -19,33 +19,34 @@ $key = $argv[1];
 $ret = readAll($key);
 var_dump($ret);
 
-
+/**
+ * @param $key
+ * @return mixed
+ */
 function readAll($key)
 {
-  $redis = Service_Dao_Redis::getInstance();
-  $input= array('key'=>$key);
-  $ret = $redis->HGETALL($input);
-  return $ret;
+    $redis = Service_Dao_Redis::getInstance();
+    $input = array('key' => $key);
+    $ret = $redis->HGETALL($input);
+    return $ret;
 }
+
 /**
  * @param $key
  * @return bool
  */
 function read($key)
 {
- $obj = new Service_Copyright_HashCache();
- $fields = array();
- for($i = 0;$i<9;$i++)
- {
-   $fields[] =  $i;
- }
- $fields[] = 'info';
- $ret = $obj->read($key,$fields);
- return $ret;
+    $obj = new Service_Copyright_HashCache();
+    $fields = array();
+    for ($i = 0; $i < 9; $i++) {
+        $fields[] = $i;
+    }
+    $fields[] = 'info';
+    $ret = $obj->read($key, $fields);
+    return $ret;
 }
 
- 
- 
- 
- /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
- ?>
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
+?>
